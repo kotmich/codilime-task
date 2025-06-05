f33bd98a913707b67b8d7bd4107a70332a773db6
Michał Kot <michalkot@Michas-Mac-Studio.local>
2025-06-05 02:44:05 +0200

    Typo fix

--- main.tf
+++ main.tf
@@ -110,7 +110,7 @@ resource "aws_lb_target_group" "alb_tg" {
   port        = 80
   protocol    = "HTTP"
   vpc_id      = aws_vpc.my_vpc.id
-  target_type = "ip'"
+  target_type = "ip"
 
   health_check {
     path                = "/"
